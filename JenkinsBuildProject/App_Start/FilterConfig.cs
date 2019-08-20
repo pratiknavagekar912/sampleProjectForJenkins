4c95dd8c0757755077e7e044e0cb9cca753eeeb2
Pratik Fakira Navagekar <pratik.navagekar@dxc.com>
2019-08-20 18:34:03 +0530

    Add project files.

---
+++ JenkinsBuildProject/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace JenkinsBuildProject
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
